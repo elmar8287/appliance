f289d99e98f4df850a715fa215d8664887063d88
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2025-10-05 18:27:38 +0000

    Added content and button CTA in ABout section

--- src/App.jsx
+++ src/App.jsx
@@ -139,14 +139,13 @@ function About() {
                             Сейчас через Matanato LLC помогаю русскоязычным предпринимателям избежать ошибок и запустить бизнес правильно.
                             
                             </p>
-                            <h4 className="font-bold text-2xl">Записаться на консультацию</h4>
+                            <div className="p-4 bg-gray-200 rounded-xl">
+                            <h4 className="font-bold text-2xl mb-2">Записаться на консультацию</h4>
                             <p>Я создал этот курс, чтобы сэкономить ваше время, уберечь от лишних расходов и распространённых ошибок, с которыми сталкиваются большинство начинающих владельцев компаний в США.</p>
-                        <a href="#leadform" className="inline-flex gap-x-1 items-center text-red-700 hover:text-red-500 duration-150 font-medium">
-                            Request a Callback
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                <path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" />
-                            </svg>
-                        </a>
+                        <a href="#leadform" className="mt-4 block py-2 px-4 text-center text-white font-medium bg-red-700 duration-150 hover:bg-red-500 active:bg-red-700 rounded-lg shadow-lg hover:shadow-none">
+                                Записаться на курс
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
